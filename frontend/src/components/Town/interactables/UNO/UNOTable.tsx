c52435039720f7dab614bb790ab1cb61d88530e1
nmungovan <nolanmichael03@yahoo.com>
2023-10-28 13:45:46 -0400

    adding types and stubs to start off the project

---
+++ frontend/src/components/Town/interactables/UNO/UNOTable.tsx
@@ -0,0 +1,7 @@
+export type UNOGameProps = {
+  gameAreaController: UNOAreaController;
+};
+
+export default function UNOTable({ gameAreaController }: UNOGameProps): JSX.Element {
+  return null;
+}
