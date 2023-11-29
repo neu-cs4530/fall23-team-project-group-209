fd649a4d4c7acf8c6047da2f43d39b2f869332c5
ayushsp <shenvipissurlenkar.a@northeastern.edu>
2023-11-29 09:08:48 -0500

    Finished MediumAi code

--- townService/src/town/games/unoAi/MediumAi.ts
+++ townService/src/town/games/unoAi/MediumAi.ts
@@ -1,71 +1,158 @@
-/*
+// eslint-disable-next-line import/no-cycle
 import UNOGame from '../UNOGame';
 
-import { Card, UNOPlayer, GameMove, UNOMove } from '../../../types/CoveyTownSocket';
+import { Card, UNOPlayer, GameMove, UNOMove, UNOSuit } from '../../../types/CoveyTownSocket';
 
-class MCTSAI {
-  root: Node;
+type ColorCount = {
+  Blue?: number;
+  Green?: number;
+  Red?: number;
+  Yellow?: number;
+  [key: string]: number | undefined;
+};
 
-  unoAreaController: UNOAreaController;
+// Define actionCardRanks with an index signature
+const actionCardRanks: { [key in UNOSuit]?: number } = {
+  '+4': 4,
+  'Wild': 3,
+  'Skip': 2,
+  'Reverse': 2,
+  '+2': 2,
+};
 
-  constructor(unoAreaController: UNOAreaController) {
-    const initialState = this.createGameStateFromController(unoAreaController);
-    this.root = new Node(initialState, null, null);
-    this.unoAreaController = unoAreaController;
+class MediumAIStrategy {
+  private _game: UNOGame;
+
+  private _aiPlayerID: string;
+
+  constructor(game: UNOGame, aiPlayerID: string) {
+    this._game = game;
+    this._aiPlayerID = aiPlayerID;
   }
 
-  createGameStateFromController(controller: UNOAreaController): GameState {
-    // Create and return a GameState object based on the current state of the game from the controller
-    // Extract and return the current game state from the controller
-    // Use controller methods like controller.ourDeck(), controller.topCard, etc.
+  public makeMove(): GameMove<UNOMove> | null {
+    const aiPlayer = this._game.state.players.find(player => player.id === this._aiPlayerID);
+
+    if (!aiPlayer || !this._game.id) {
+      return null;
+    }
+
+    // Enhanced logic for smarter decision making
+    const bestMove = this._determineBestMove(aiPlayer.cards);
+
+    if (!bestMove) {
+      // No valid move found, draw a card
+      this._game.drawCard(this._aiPlayerID);
+      return this.makeMove();
+    }
+
+    return {
+      playerID: this._aiPlayerID,
+      gameID: this._game.id,
+      move: { card: bestMove, player: this._aiPlayerID },
+    };
   }
 
-  selectMove(): Move {
-    // Main method to perform MCTS steps
+  private _determineBestMove(cards: Card[]): Card | null {
+    // Early exit if no cards are available
+    if (cards.length === 0) {
+      return null;
+    }
 
-    // 1. Selection
-    // 2. Expansion
-    // 3. Simulation
-    // 4. Backpropagation
+    const allPlayers = this._game.state.players;
+    const currPlayerIndex = this._game.state.currentPlayerIndex;
 
-    // Return the best move after running MCTS for a certain number of iterations or time
-    for (let i = 0; i < numberOfIterations; i++) {
-      let node = this.selectNode(this.root);
-      if (!node.isFullyExpanded()) {
-        node = this.expandNode(node);
+    // Count cards of each color in hand
+    const colorCount: ColorCount = cards.reduce((count, card) => {
+      if (card.color !== 'Wildcard') {
+        count[card.color] = (count[card.color] || 0) + 1;
       }
-      const outcome = this.simulateRandomPlay(node);
-      this.backpropagate(node, outcome);
+      return count;
+    }, {} as ColorCount); // Initialize colorCount as an empty object of type ColorCount
+
+    // Sort the cards to prioritize action and wild cards, and then by the most common color in hand
+    const sortedCards = cards.slice().sort((a, b) => {
+      // Prioritize action cards and wild cards
+      const actionPriority = this._getActionCardPriority(b) - this._getActionCardPriority(a);
+      if (actionPriority !== 0) return actionPriority;
+
+      // Next, prioritize the most common color in hand
+      const colorPriority = (colorCount[b.color] || 0) - (colorCount[a.color] || 0);
+      return colorPriority;
+    });
+
+    // Find a card that can be played
+    for (const card of sortedCards) {
+      if (this._game._validCard(card)) {
+        // Check if playing this card is beneficial based on the current game state
+        if (this._isBeneficialMove(card, allPlayers, currPlayerIndex)) {
+          return card;
+        }
+      }
     }
 
-    // Select the best move from the root node based on the results of the MCTS
-    return this.bestMoveFromRoot();
+    // No beneficial card found
+    return null;
   }
 
-  selectNode(node: Node): Node {
-    while (node.isFullyExpanded()) {
-      node = node.selectChild();
+  private _getActionCardPriority(card: Card): number {
+    // Higher number means higher priority
+    // Use the rank of the card to get its priority, defaulting to 0 if not found
+    return actionCardRanks[card.rank] || 0;
+  }
+
+  private _isBeneficialMove(
+    card: Card,
+    players: ReadonlyArray<UNOPlayer>,
+    currentPlayerIndex: number,
+  ): boolean {
+    const nextPlayerIndex = (currentPlayerIndex + 1) % players.length;
+    const nextPlayer = players[nextPlayerIndex];
+    const criticalCardCount = 2; // Threshold for considering a player close to winning
+
+    // Check if the next player has few cards and prioritize action cards to disrupt their turn
+    if (
+      ['Skip', 'Reverse', '+2', '+4'].includes(card.rank.toString()) &&
+      nextPlayer.cards.length <= criticalCardCount
+    ) {
+      return true; // Beneficial to disrupt a player close to winning
     }
-    return node;
-  }
 
-  expandNode(node: Node): Node {
-    // Choose a move that has not been explored yet and create a new node
-  }
+    // Additional beneficial move checks:
 
-  // Run a simulated game from a given node using random plays until a terminal state
-  simulateRandomPlay(node: Node): GameOutcome {
-    // Randomly play out a game from the node's state until the end
-    // Return the outcome (win/lose)
-  }
+    // 1. If the AI has multiple cards of the same color, it might be beneficial to play one to reduce hand size
+    const sameColorCards = players[currentPlayerIndex].cards.filter(c => c.color === card.color);
+    if (sameColorCards.length > 2) {
+      return true; // Beneficial to play cards of the same color
+    }
 
-  // Update the nodes from the given node up to the root based on the simulation outcome
-  backpropagate(node: Node, outcome: GameOutcome): void {
-    // Update wins and simulations for each node in the path
-  }
+    // 2. If the AI has a Wild card but also has other playable cards, it might save the Wild card for a more critical situation
+    if (
+      card.rank === 'Wild' &&
+      players[currentPlayerIndex].cards.some(c => this._game._validCard(c) && c.rank !== 'Wild')
+    ) {
+      return false; // Avoid using Wild too early
+    }
 
-  bestMoveFromRoot(): Move {
-    // Choose the move from the root node that has the best win ratio or most simulations
+    // 3. Prioritize playing action cards if the AI has many cards, to change gameplay dynamics
+    if (
+      players[currentPlayerIndex].cards.length > 4 &&
+      ['Skip', 'Reverse', '+2'].includes(card.rank.toString())
+    ) {
+      return true; // Beneficial to play action cards when AI has many cards
+    }
+
+    // 4. Consider saving Wild and +4 cards for critical situations
+    if (
+      (card.rank === 'Wild' || card.rank === '+4') &&
+      players.some(player => player.cards.length > 2)
+    ) {
+      return false; // Save these cards for later unless a player is close to winning
+    }
+
+    // Default to true if no specific condition is met
+    return true;
   }
 }
-*/
+
+export default MediumAIStrategy;
